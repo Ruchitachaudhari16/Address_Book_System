31b50f6149332b75ea866b05186c18a1337d524e
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-03-03 22:40:35 -0500

    [Ruchita] Add:  new contact created

--- DAy_9_AddressBook/DAy_9_AddressBook/Address.cs
+++ DAy_9_AddressBook/DAy_9_AddressBook/Address.cs
@@ -8,37 +8,41 @@ namespace DAy_9_AddressBook
 {
     internal class Address
     {
+        //create list of field contact
+      //public List<Contact> contactList;
+
         //Ability to create a Contacts in AddressBook with first and last names, address, city, state, zip, phone number andemail...
         public string First_name;
         public string Last_name;
         public string A_Address;
         public string City;
         public string State;
-        public string Zip;
-        public string Phone_number;
+        public int Zip;
+        public int Phone_number;
         public string Email;
 
-        public Address( string name,string surname,string addres,string city,string state,string zip,string phone,string email)
+        public Address(string fname, string lname, string address, string city, string state, int zip, int phone_no, string email)
         {
-           this.First_name = name;
-            this.Last_name = surname;
-            this.A_Address = addres;
+            this.First_name = fname;
+            this.Last_name = lname;
+            this.A_Address = address;
             this.City = city;
             this.State = state;
             this.Zip = zip;
-            this.Phone_number = phone;
+            this.Phone_number = phone_no;
             this.Email = email;
         }
-         public void display()
-         {
+        public  void display()
 
-             Console.WriteLine( First_name + ""  + Last_name);
-             Console.WriteLine(A_Address + "" + City);
-             Console.WriteLine(City + " " + State);
-             Console.WriteLine(Zip + " " + Phone_number);
-             Console.WriteLine(Email);
-         }
-    
-    
+        { 
+            Console.WriteLine(this.First_name + "" + this.Last_name);
+            Console.WriteLine(this.A_Address + "" + this.City);
+            Console.WriteLine(this.City + " " + this.State);
+            Console.WriteLine(this.Zip + " " + this.Phone_number);
+            Console.WriteLine(this.Email);
+        }
+
+
     }
 }
+    
--- DAy_9_AddressBook/DAy_9_AddressBook/Program.cs
+++ DAy_9_AddressBook/DAy_9_AddressBook/Program.cs
@@ -1,15 +1,47 @@
-﻿namespace DAy_9_AddressBook
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Versioning;
+using System.Xml.Linq;
+
+namespace DAy_9_AddressBook
 {
     internal class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Address a1 = new Address("niki","kale", "karve_road", "Pune", "Maharasatra", "12345", "9130401234", "abc@gmail.com");
+            Program p1= new Program();
+            Console.WriteLine("Welcome to addressBook ");
+            Console.WriteLine("Enter firstname");
+            string fname = Console.ReadLine();
 
-            
-            a1.display();
-            
-            
+            Console.WriteLine("Enter Lastname");
+            string lname = Console.ReadLine();
+
+            Console.WriteLine("Enter Address");
+            string address = Console.ReadLine();
+
+          
+            Console.WriteLine("Enter city");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("Enter state");
+            string state = Console.ReadLine();
+
+            Console.WriteLine("Enter Zip");
+            int zip = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("phone number");
+            int phon_no = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("email");
+            string email = Console.ReadLine();
+
+            Address A1 = new Address(fname, lname, address, city, state,zip, phon_no, email);
+            A1.display();
+            Console.ReadLine();
+           
+
         }
     }
 }
